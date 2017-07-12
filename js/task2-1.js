47fd80b89ff02dd6391af1985627e35b10954c99
matteokjh <429797371@qq.com>
2017-07-12 21:10:31 +0800

    “ife task

---
+++ js/task2-1.js
@@ -0,0 +1,15 @@
+﻿window.onload = function() {
+    /*	
+    在注释下方写下代码
+    给按钮button绑定一个点击事件
+    在事件处理函数中
+    获取aqi-input输入的值，并显示在aqi-display中
+    */
+    var input = document.getElementById("aqi-input");
+    var button = document.getElementsByTagName("button");
+    var display= document.getElementById("aqi-display");
+    button[0].addEventListener('click', function () {
+        console.log(input.value);
+        display.innerHTML = input.value;
+    })
+}
---
+++ js/task2-2.js
@@ -0,0 +1,46 @@
+﻿var aqiData = [
+  ["北京", 90],
+  ["上海", 50],
+  ["福州", 10],
+  ["广州", 50],
+  ["成都", 90],
+  ["西安", 100]
+];
+var newData = Array();
+(function () {
+
+    /*
+    在注释下方编写代码
+    遍历读取aqiData中各个城市的数据
+    将空气质量指数大于60的城市显示到aqi-list的列表中
+    */
+
+    //for (i = 0,j = 0; i <= 5; i++){
+    //    if (aqiData[i][1] > 60) {
+    //        newData[j] = aqiData[i];
+    //        j++;
+    //    }
+    //}//挑合格的出来。
+    newData = aqiData.filter(aqi => aqi[1] > 60);
+    //newData.sort(function (a, b) {
+    //    return b[1] - a[1];
+    //});//sort
+    newData.sort((a,b) => b[1]-a[1]);
+    var number = ["一", "二", "三", "四"];
+    
+    var ul = document.getElementById("aqi-list");
+    //for (var i = 0; i < newData.length; i++) {
+        
+    //    ul.appendChild(li);
+    //    li.innerHTML = "第" + number[i] + "名" + ":" + newData[i][0] + "," + newData[i][1];
+    //    console.log(newData[i]);
+    //}
+    
+    newData.forEach( (data, idx) => {
+        var li = document.createElement("li");
+        ul.appendChild(li);
+        li.innerHTML = "第" + number[idx] + "名" + ":" + data[0] + "," + data[1];
+        console.log(data);
+    })
+
+})();
